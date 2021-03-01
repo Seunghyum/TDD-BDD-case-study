976afefc81553590d93811448517c567e00f9c8a
seunghyum <antmgu@gmail.com>
2021-03-01 18:07:08 +0900

    chore: set jest config

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,13 @@ module.exports = {
     '^.+\\.(js|jsx)?$': 'babel-jest',
     '^.+\\.(ts|tsx)?$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+      '<rootDir>/src/__mocks__/fileMock.js',
+    '^~src(.*)$': '<rootDir>/src$1',
+    '^~components(.*)$': '<rootDir>/src/components$1',
+    '^~pages(.*)$': '<rootDir>/src/pages$1',
+  },
   testMatch: [
     '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
     '<rootDir>/**/*.spec.(js|jsx|ts|tsx)',
