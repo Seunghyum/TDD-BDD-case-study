c82c1b66262e975013c19a7259f6e7e5584d7e81
seunghyum <antmgu@gmail.com>
2021-03-02 23:51:22 +0900

    close #4; docs: update about 서버 API 요청로직(useLifestyleList)

--- src/containers/LifestyleCardListContainer.tsx
+++ src/containers/LifestyleCardListContainer.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement, useEffect } from 'react';
+import React, { ReactElement } from 'react';
 import styled from 'styled-components';
 
 import LifestyleCard from '~src/components/LifestyleCard';
