137daba7da2ea44a90abbdd06811e7f472fe12d5
seunghyum <antmgu@gmail.com>
2021-03-04 20:55:06 +0900

    feat: add data-testid attribute to LifestyleCardListContainer for test infinite scroll

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
 import Container from '~src/components/Container';
 import BookmarksContainer from '~src/containers/BookmarksContainer';
 import LifestyleCardListContainer from '~src/containers/LifestyleCardListContainer';
-
-const CardListBlock = styled.div`
-  display: flex;
-  flex-wrap: wrap;
-  align-items: flex-start;
-  margin: 0 -10px;
-`;
 
 const ListPage: React.FC = () => {
   const [isOnlyBookmarks, setIsOnlyBookmarks] = useState(false);
@@ -21,7 +13,7 @@ const ListPage: React.FC = () => {
   return (
     <Container>
       <button onClick={handleBookmarkFilter}>북마크만</button>
-      <CardListBlock>{isOnlyBookmarks ? <BookmarksContainer /> : <LifestyleCardListContainer />}</CardListBlock>
+      {isOnlyBookmarks ? <BookmarksContainer /> : <LifestyleCardListContainer />}
     </Container>
   );
 };
